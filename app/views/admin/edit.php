69ab2844d943e4bdce5e54530a2cb89761e8a34e
veaceslav <filed12345@gmail.com>
2023-05-17 19:25:25 +0300

    update deletebtn

--- app/views/admin/edit.php
+++ app/views/admin/edit.php
@@ -22,6 +22,15 @@
                 <textarea name="body"><?= $news['body'] ?></textarea>
                 <?php if (isset($errors['body'])) echo "<p class='error-message'>{$errors['body']}</p>"; ?>
 
+                <!-- Comments Section -->
+                <label>Comments:</label>
+                <?php foreach ($news['comments'] as $comment): ?>
+                    <div class="comment">
+                        <p><?= $comment['comment'] ?></p>
+                        <button type="button" class="delete-comment-btn" onclick="confirmDeleteComment('<?= $comment['id'] ?>')">Delete Comment</button>
+                    </div>
+                <?php endforeach; ?>
+
                 <label>Current Image:</label>
                 <img src="/NewsWebSitePhp/public/uploads/<?= $news['media'] ?>" alt="<?= $news['title'] ?>">
 
@@ -31,21 +40,37 @@
 
                 <input type="submit" class="update-btn" value="Update News">
             </form>
-
-            <!-- Comments Section -->
-            <label>Comments:</label>
-            <?php foreach ($news['comments'] as $comment): ?>
-                <div class="comment">
-                    <p><?= $comment['comment'] ?></p>
-                    <form method="POST" action="/NewsWebSitePhp/public/deleteComment/<?= $news['id'] ?>/<?= $comment['id'] ?>">
-                        <input type="hidden" name="comment_id" value="<?= $comment['id'] ?>">
-                        <input type="submit" class="delete-comment-btn" value="Delete Comment">
-                    </form>
-                </div>
-            <?php endforeach; ?>
         </div>
     </div>
 </div>
+
+<!-- Delete Comment Modal -->
+<div id="deleteCommentModal" class="modal-mask" style="display: none;">
+    <div class="modal-wrapper">
+        <div class="modal-content">
+            <h2>Are you sure you want to delete this comment?</h2>
+            <form id="deleteCommentForm" method="POST">
+                <button type="submit">Confirm Delete</button>
+                <button type="button" onclick="closeCommentModal()">Cancel</button>
+            </form>
+        </div>
+    </div>
+</div>
+
 <?php require '../app/views/footer.php'; ?>
 </body>
+<script>
+    let deleteCommentModal = document.getElementById('deleteCommentModal');
+    let deleteCommentForm = document.getElementById('deleteCommentForm');
+
+    function confirmDeleteComment(id) {
+        deleteCommentForm.action = "/NewsWebSitePhp/public/deleteComment/<?= $news['id'] ?>/" + id;
+        deleteCommentModal.style.display = "flex";
+    }
+
+    function closeCommentModal() {
+        deleteCommentModal.style.display = "none";
+    }
+</script>
 </html>
+
